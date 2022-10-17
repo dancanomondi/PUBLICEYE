c2550fd5d69b6d6f3b55b577a3bf18436d919687
dancanomondi <dancan.omondi6831@gmail.com>
2022-10-17 19:40:18 +0300

    Added new project files

--- publiceye/src/App.js
+++ publiceye/src/App.js
@@ -1,11 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+import Users from './users/pages/Users';
+import SignUp from './users/components/signup';
+import Login from './users/components/login';
+import UserCrime from './users/components/userCrime';
+import HowItWorks from './shared/howItWorks/howItWorks';
+import HomePage from './shared/pages/HomePage/HomePage';
+import Crime from './crime/components/Crime';
+import MainNavigation from './shared/pages/MainNavigation/MainNavigation';
+import Modal from './shared/components/Modal/Modal';
+
+
+const App = () =>{
   return (
-    <div className="App">
-      <h1>public Eye </h1>
-    </div>
+    <Router>
+          <Routes>
+              <Route path='/' element={<HomePage/>}/>
+              <Route path='/signup' exact element = {<SignUp/>}/>
+              <Route path='/login' exact element = {<Login/>}/>
+              <Route path='/crime' exact element = {<UserCrime/>}/>
+              <Route path='/crimes' exact element = {<Users/>}/>
+              <Route path='/howitworks' exact element = {<HowItWorks/>}/>
+              <Route path='/crimes/:id' exact element = {<Crime/>}/>
+              <Route path='/modal' exact element = {<Modal/>}/>
+          </Routes>
+                
+    </Router>
+     
   );
+
 }
+
 
 export default App;
